4b296287446d523ee83b07b9d8039af64838a78d
João Gabriel <joao.tonaco@proton.me>
2024-04-04 09:53:04 -0300

    create customers schemas and typings

---
+++ src/types/customers.ts
@@ -0,0 +1,14 @@
+import type { z } from "zod";
+import type {
+	createCustomerPayloadSchema,
+	createCustomerResponseSchema,
+	createCustomerSchema,
+} from "../assertions/customers";
+
+export type CreateCustomerType = z.infer<typeof createCustomerSchema>;
+
+export type CreateCustomerPayload = z.infer<typeof createCustomerPayloadSchema>;
+
+export type CreateCustomerResponse = z.infer<
+	typeof createCustomerResponseSchema
+>;
